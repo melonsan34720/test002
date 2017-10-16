ebe25166c02f240ed44e311de6d6f61aaf506cce
chito <chito@DESKTOP-K6J1O37>
2017-10-16 17:14:22 +0900

    first commit

---
+++ src/test002/Test002.java
@@ -0,0 +1,18 @@
+package test002;
+
+import java.util.Scanner;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("input Number > ");
+		//int n = Integer.parseInt(scan.next());
+		double m = Double.parseDouble(scan.next());
+		
+		//System.out.println("Number = "+n);
+		System.out.println("RealNumber = "+m);
+	}
+
+}
